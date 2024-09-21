d9c35685aeedf2448a7dc741f45eb7e4d103f19d
Meth Munindradasa <methsndr@gmail.com>
2024-09-21 22:26:23 +1000

    Structure of dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+export default function Home() {
+  
+  const loremText = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi quas unde hic quo rerum ab officia cupiditate sequi eligendi porro consequatur dolore minima, consequuntur esse accusantium eveniet? Earum, dolorum molestias.'
+
+  return (
+    <div>
+      <h1 className="mx-8 my-8">Dashboard</h1>
+
+      <div className="flex justify-center ">
+        <div className="w-1/4">
+          <h2>Summary of risks</h2>
+          <p className="">{loremText}</p>
+        </div>
+        <div>
+          <h2>Health safety meter</h2>
+        </div>
+      </div>
+
+
+      <div className="my-8 flex justify-center">
+        <p>Environment Visual</p>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="w-1/4">
+          <h2>Advanced report</h2>
+          <p>loremText</p>
+        </div>
+
+        <div>
+          <h2>3D embed</h2>
+        </div>
+      </div>
+
+    </div>
+  );
+}
